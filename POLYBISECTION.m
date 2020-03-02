8ed7f3f05eac7613cf33e4c6bf316572a34bfb9a
sophiaibanez <57376444+sophiaibanez@users.noreply.github.com>
2020-03-02 21:02:49 +0800

    Update and rename polybisection.m to POLYBISECTION.m

--- POLYBISECTION.m
+++ POLYBISECTION.m
@@ -1,23 +1,23 @@
-function [r,k]=polybisection(coef,ab,epsilon)
-a=ab(1);
-b=ab(2);
-fa=polyval(coef,a);
-fb=polyval(coef,b);
+function [r,k] = polybisection(coef,ab,epsilon)
+a = ab(1);
+b = ab(2);
+fa = polyval(coef,a);
+fb = polyval(coef,b);
 
 fc = inf;
-k=0;
+k = 0;
 
-if fa*fb>0
+if fa*fb > 0
     error('error interval')
     
 else
-    while abs(fc)>epsilon 
+    while abs(fc) > epsilon 
     
         c = (a+b)/2;
-        fc=polyval(coef,c);
+        fc = polyval(coef,c);
         
-        r=[a,b];
-        k=k+1;
+        r = [a,b];
+        k = k+1;
 
         if fc < 0
             if fa < 0
